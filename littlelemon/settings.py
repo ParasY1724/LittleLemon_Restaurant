d983dc26e8196152d203311befed620ed5990a9e
ParasY174 <parasyerunkar95@gmail.com>
2024-09-25 23:27:10 +0530

    MYSQL CONNECTIVITY

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'your_database_name',
+        'USER': 'your_username',
+        'PASSWORD': 'your_password',
+        'HOST': 'your_host',
+        'PORT': '3306',
     }
 }
+
 
 # The settings for media files have been updated for the Graded assessment
 MEDIA_URL = '/media/'
