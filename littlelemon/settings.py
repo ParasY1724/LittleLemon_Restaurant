c49f93fdc2a373ad6257d4e7079d22c55484b74f
Pratik-Patil3800 <123655926+Pratik-Patil3800@users.noreply.github.com>
2024-10-05 23:15:47 +0530

    Update settings.py

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -74,8 +74,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
